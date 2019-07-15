1976e80fca75bce794d4e2629fe130bd5c9087fa
Anubhav311 <anubhavbhambri1@gmail.com>
2019-07-16 04:59:18 +0530

    passed test for binary_search_recursive

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -13,30 +13,8 @@ def linear_search(arr, target):
 
 
 # STRETCH: write an iterative implementation of Binary Search 
-
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(len(arr)):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        for j in range(i+1, len(arr)):
-            if arr[smallest_index] > arr[j]:
-                smallest_index = arr.index(arr[j])
-
-        # TO-DO: swap
-        temp = arr[cur_index]
-        arr[cur_index] = arr[smallest_index]
-        arr[smallest_index] = temp
-
-    return arr
-
-
 def binary_search(arr, target):
-
-  selection_sort(arr)
-
+  
   if len(arr) == 0:
     return -1 # array empty
     
@@ -58,13 +36,23 @@ def binary_search(arr, target):
 
   return -1 # not found
 
-# binary_search([5,8,9,-9,1,2,0], 6)
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-  middle = (low+high)//2
+  average = (low+high)//2
 
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+
+  if arr[average] == target:
+    print('if: ', low, high, average)
+    return average
+  elif arr[average] > target:
+    print('elif: ', low, high, average)
+    return binary_search_recursive(arr, target, low = low, high = average)
+  else:
+    print('else: ', low, high, average)
+    return binary_search_recursive(arr, target, low = average, high = high)
+
